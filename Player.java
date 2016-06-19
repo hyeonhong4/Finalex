e46ebdb53ccf04182b9ed41f4036d54a0d93cb30
hyeonhong4 <gusghd14@naver.com>
2016-06-20 04:14:13 +0900

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,19 +2,24 @@ import java.util.*;
 public class Player{
   int money;
   int zen;
-  ArrayList Big;
-  ArrayList Small;
+  ArrayList big_temple;
+  ArrayList small_temple;
   int dog;
   String dog_name;
   int dog_choice;
   int dog_love;
-  public Player(int money, int zen,ArrayList Big,ArrayList Small,int dog,String dog_name, int dog_choice,int dog_love){
+  int write;
+  
+  public Player(int money, int zen,ArrayList big_temple,ArrayList small_temple,int dog,String dog_name, int dog_choice,int dog_love,int write){
     this.money=money;
     this.zen=zen;
-    this.Small=Small;
-    this.Big=Big;
+    this.small_temple=small_temple;
+    this.big_temple=big_temple;
     this.dog=dog;
     this.dog_name=dog_name;
     this.dog_choice=dog_choice;
     this.dog_love=dog_love;
-  }}
+    this.write=write;
+  
+  }
+}
