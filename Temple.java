012d52777676c9c91bc732103f46a5b3d42c1fd0
hyeonhong4 <gusghd14@naver.com>
2016-06-20 04:14:59 +0900

    Update Temple.java

--- Temple.java
+++ Temple.java
@@ -7,36 +7,59 @@ public class Temple{
     System.out.println("Help Someone -10$ -> Press 2");
     System.out.println("Work at Company +$50, -30 Zen -> Press 3");
     System.out.println("Purchase Temple -> Press 4");
+    System.out.println("Write a Profanity -> Press 5");
+    
     if(x.dog==0){
-      System.out.println("Buy Dog -$30 -> Press 5");
+      System.out.println("Buy Dog -$30 -> Press 6");
     }
+    
     if(x.dog>=1){
-      System.out.println("Take care of "+x.dog_name+" -> Press 5");
+      System.out.println("Take care of "+x.dog_name+" -> Press 6");
     }
+    
     if(x.dog==1){
-      System.out.println("Dress up Dog -$30 -> Press 6");
+      System.out.println("Dress up Dog -$30 -> Press 7");
     }
     
     while(true){
       Scanner player_number = new Scanner(System.in);
       System.out.print("");
       int player_choice = player_number.nextInt();
-      if (player_choice<=6 && player_choice >0){
+    
+      if (player_choice<=7 && player_choice >0){
+      
         if (player_choice==1){
           x.zen=x.zen+1;
         }
+        
         else if (player_choice==2){
           x.money=x.money-10;
           x.zen=x.zen+5;
         }        
+        
         else if (player_choice==3){
           x.money=x.money+50;
           x.zen=x.zen-30;
         }
+        
         else if (player_choice==4){
           Purchase collect = new Purchase(x);
         }
+        
         else if (player_choice==5){
+          x.write++;
+          System.out.println("Write...");
+          System.out.println("Profanity "+x.write+"/5");
+        
+          if(x.write==5){
+          System.out.println("Complete. + 20 Zen");
+          x.zen=x.zen+20;
+          x.write=0;
+          }
+        }
+        
+        else if (player_choice==6){
+        
           if(x.dog==0){
             System.out.println("You Go Pet Shop and Buy Dog");
             x.dog=x.dog+1;
@@ -46,19 +69,23 @@ public class Temple{
             x.dog_name=dogname;
             x.money=x.money-30;
           }
+          
           else{
             x.dog_choice++;
             break;
           }
         }
-        else if (player_choice==6 && x.dog==1){
+        
+        else if (player_choice==7 && x.dog==1){
           System.out.println(x.dog_name+" Dress Up!");
           x.money=x.money-30;
           x.dog++;
         }
+        
         else{
           continue;}
       }
       break;
     }
-  }}
+  }
+}
