17b220e1e5dab4b17a966b6b34d5c9f1deb28396
hyeonhong4 <gusghd14@naver.com>
2016-06-20 01:31:07 +0900

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Player{
+  int money;
+  int zen;
+  ArrayList Big;
+  ArrayList Small;
+  int dog;
+  String dog_name;
+  int dog_choice;
+  int dog_love;
+  public Player(int money, int zen,ArrayList Big,ArrayList Small,int dog,String dog_name, int dog_choice,int dog_love){
+    this.money=money;
+    this.zen=zen;
+    this.Small=Small;
+    this.Big=Big;
+    this.dog=dog;
+    this.dog_name=dog_name;
+    this.dog_choice=dog_choice;
+    this.dog_love=dog_love;
+  }}
